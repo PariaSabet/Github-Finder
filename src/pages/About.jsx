832f730c9bb2e160c9792fa464346e7bbd7c04aa
Paria <paria20@gmail.com>
2022-11-06 15:23:41 +0330

    added the  basics

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        {" "}
+        A React app to search Github profiles and see profile details.
+      </p>
+      <p className="text-lg text-gray-400">
+        {" "}
+        Version <span className="text-white">1.0.0</span>
+      </p>
+    </div>
+  );
+}
+
+export default About;
